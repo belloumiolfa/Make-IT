a4f419d4053786399db99d94fa3d75c7972aee81
belloumiolfa <bellolfa@gmail.com>
2021-02-08 04:52:27 -0800

    Version 1.0.0

--- Back-end/server.js
+++ Back-end/server.js
@@ -26,8 +26,8 @@ var app = express();
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
    //set static folder
-   app.use(express.static(path.join(__dirname, 'my-app/build')));
-   app.get('*', (res, req) => {
+   app.use(express.static(path.join(__dirname, '/my-app/build')));
+   app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'my-app', 'build', 'index.html'));
    });
 } else {
